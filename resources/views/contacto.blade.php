af78ef0c025b77aa992e16375d6d2e2b4419cbc6
camilaN <cnp005@alumnos.ucn.cl>
2019-05-06 21:05:18 -0400

    Nuevo avance seccion consulta

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -18,23 +18,45 @@
                 <span class="caret"></span></button>
             <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
                 <!--<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Consultas</a></li>-->
-                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Consultas</button>
-                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo1">Voluntarios</button>
+                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Consulta</button>
+                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo1">Voluntario</button>
 
                 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Voluntarios</a></li>
                 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Denuncia</a></li>
                 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Otros</a></li>
                 <li role="presentation" class="divider"></li>
-                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">FAQ</a></li>
+
+
             </ul>
+
+            <!--Formulario Consulta-->
             <div id="demo" class="collapse">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                <form name="FormularioConsulta" method="post">
+                    <br />
+                    <p>Asegurece que su consulta no se encuentre en <a> <!--href="enlacepagina.html"-->FAQ</a></p>
+                    <br />
+
+                    <label for="name" class="col-sm-2 col-form-label">Nombre</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="name" placeholder="name">
+                    </div>
+
+                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    </div>
+
+                    <br />
+                    <textarea name="comentarios" rows="10" cols="40">Escribe aquí tu consulta</textarea>
+
+                </form>
             </div>
+
+            <!-- Formulario Voluntarios-->
             <div id="demo1" class="collapse">
                 wena wena
             </div>
+
         </div>
 
     </div>
