54f401a04e7e8c5e5e78a6231455995bdb2cc5bd
Manuel <manuelzuletab@gmail.com>
2019-05-08 17:03:36 -0400

    implementation layout general

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,100 @@
+<!-- Footer -->
+<footer class="page-footer font-small indigo">
+
+    <!-- Footer Links -->
+    <div class="container">
+
+        <!-- Grid row-->
+        <div class="row text-center d-flex justify-content-center pt-5 mb-3">
+            <span style="font-size: 20px;color: #BABDC2">
+                Colaboradores y Alianzas
+            </span>
+        </div>
+        <!-- Grid row-->
+        <hr class="rgba-white-light" style="margin: 0 20%;background-color: #596070">
+
+        <!-- Grid row-->
+        <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
+
+            <!-- Grid column -->
+            <div class="col-md-12 col-12 mt-5">
+                <div style="text-align: center;">
+                    <a href="http://sordosiguales.net/" rel="home">
+                        <img class = "img" src="{{"/storage/Test logo UCN.png"}}" style ="width:150px;height: 150px; padding: 15px;" alt="SordosIguales">
+                    </a>
+                    <a href="http://sordosiguales.net/" rel="home">
+                        <img class = "img" src="{{"/storage/Test logo UCN.png"}}" style ="width:150px;height: 150px; padding: 15px;" alt="SordosIguales">
+                    </a>
+                    <a href="http://sordosiguales.net/" rel="home">
+                        <img class = "img" src="{{"/storage/Test logo UCN.png"}}" style ="width:150px;height: 150px; padding: 15px;" alt="SordosIguales">
+                    </a>
+                    <a href="http://sordosiguales.net/" rel="home">
+                        <img class = "img" src="{{"/storage/Test logo UCN.png"}}" style ="width:150px;height: 150px; padding: 15px;" alt="SordosIguales">
+                    </a>
+                    <a href="http://sordosiguales.net/" rel="home">
+                        <img class = "img" src="{{"/storage/Test logo UCN.png"}}" style ="width:150px;height: 150px; padding: 15px;" alt="SordosIguales">
+                    </a>
+                    <a href="http://sordosiguales.net/" rel="home">
+                        <img class = "img" src="{{"/storage/Test logo UCN.png"}}" style ="width:150px;height: 150px; padding: 15px;" alt="SordosIguales">
+                    </a>
+                    <a href="http://sordosiguales.net/" rel="home">
+                        <img class = "img" src="{{"/storage/Test logo UCN.png"}}" style ="width:150px;height: 150px; padding: 15px;" alt="SordosIguales">
+                    </a>
+                    <a href="http://sordosiguales.net/" rel="home">
+                        <img class = "img" src="{{"/storage/Test logo UCN.png"}}" style ="width:150px;height: 150px; padding: 15px;" alt="SordosIguales">
+                    </a>
+                    <a href="http://sordosiguales.net/" rel="home">
+                        <img class = "img" src="{{"/storage/Test logo UCN.png"}}" style ="width:150px;height: 150px; padding: 15px;" alt="SordosIguales">
+                    </a>
+                    <a href="http://sordosiguales.net/" rel="home">
+                        <img class = "img" src="{{"/storage/Test logo UCN.png"}}" style ="width:150px;height: 150px; padding: 15px;" alt="SordosIguales">
+                    </a>
+                    <a href="http://sordosiguales.net/" rel="home">
+                        <img class = "img" src="{{"/storage/Test logo UCN.png"}}" style ="width:150px;height: 150px; padding: 15px;" alt="SordosIguales">
+                    </a>
+                    <a href="http://sordosiguales.net/" rel="home">
+                        <img class = "img" src="{{"/storage/Test logo UCN.png"}}" style ="width:150px;height: 150px; padding: 15px;" alt="SordosIguales">
+                    </a>
+                </div>
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row-->
+        <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
+
+        <!-- Grid row-->
+        <div class="row pb-3 pt-5">
+
+            <!-- Grid column -->
+            <div class="col-md-12">
+
+                <div class="mb-5 flex-center align-self-center" style ="color: #BABDC2;text-align: center">
+                    <!-- Facebook -->
+                    <a class="fb-ic footerLink" href = "monkey.png"><i class="fab fa-facebook-f fa-lg white-text mr-4"></i></a>
+                    <!-- Twitter -->
+                    <a class="tw-ic footerLink" href = "monkey.png"><i class="fab fa-twitter fa-lg white-text mr-4"></i></a>
+                    <!--Instagram-->
+                    <a class="ins-ic footerLink" href = "monkey.png"><i class="fab fa-instagram fa-lg white-text mr-4"></i></a>
+                    <!--Whatsapp-->
+                    <a class="pin-ic footerLink" href = "monkey.png"><i class="fab fa-whatsapp fa-lg white-text"></i></a>
+
+                </div>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row-->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3" style="background-color: #131B2A;color: #BABDC2">© 2019 Copyright:
+        <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
