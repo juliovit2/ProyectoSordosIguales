5f3c04e3b80b15e012bdb26bc53abc8c4b7e7a18
Ignacio <Icm011@alumnos.ucn.cl>
2019-05-09 22:01:42 -0400

    Modificacion Contacto checkbox

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -2,65 +2,101 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-    </head>
-    <body>
+<div class="col-md-4">
+    <div class="col-md-6">
+        <h4>Radio Buttons</h4>
+        <!-- Script que hace la funcion de show and hide -->
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $(".consulta").click(function (evento) {
+                    var valor = $(this).val();
 
-    <div class="container">
-        <h2>Seleccione Tipo de Consulta</h2>
-        <div class="dropdown">
-            <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Seleccionar
-                <span class="caret"></span></button>
-            <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-                <!--<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Consultas</a></li>-->
-                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Consulta</button>
-                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo1">Voluntario</button>
+                    if (valor == '1'){
+                        $("#content").css("display","block");
+                        $("#content2").css("display","none");
+                    }else {
+                        $("#content").css("display","none");
+                        $("#content2").css("display","block");
+                    }
+                })
 
-                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Voluntarios</a></li>
-                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Denuncia</a></li>
-                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Otros</a></li>
-                <li role="presentation" class="divider"></li>
+            })
+
+        </script>
+        <!-- MENU con checkbox-->
+        <div class="funkyradio">
+
+            <div class="funkyradio-default">
 
 
-            </ul>
-
-            <!--Formulario Consulta-->
-            <div id="demo" class="collapse">
-                <form name="FormularioConsulta" method="post">
-                    <br />
-                    <p>Asegurece que su consulta no se encuentre en <a> <!--href="enlacepagina.html"-->FAQ</a></p>
-                    <br />
-
-                    <label for="name" class="col-sm-2 col-form-label">Nombre</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="name" placeholder="name">
+                <input type="radio" name="radio" class="consulta" id="radio1" value="1"  />
+                <label for="radio1">First Option default</label>
+                <!-- Form -->
+                <div id="content" style="display: none;">
+                    <!-- Lo que se muestra del formulario en la página -->
+                    <div class="container">
+                        <h2>Horizontal form</h2>
+                        <form class="form-horizontal" action="/action_page.php">
+                            <div class="form-group">
+                                <label class="control-label col-sm-2" for="email">Email:</label>
+                                <div class="col-sm-10">
+                                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-sm-2" for="pwd">Password:</label>
+                                <div class="col-sm-10">
+                                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <div class="checkbox">
+                                        <label><input type="checkbox" name="remember"> Remember me</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <button type="submit" class="btn btn-default">Submit</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
+                </div>
 
-                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                    </div>
-
-                    <br />
-                    <textarea name="comentarios" rows="10" cols="40">Escribe aquí tu consulta</textarea>
-
-                </form>
             </div>
-
-            <!-- Formulario Voluntarios-->
-            <div id="demo1" class="collapse">
-                wena wena
+            <div class="funkyradio-primary">
+                <input type="radio" name="radio" class="consulta" id="radio2" value="2" />
+                <label for="radio2">Second Option primary</label>
+                <!-- Form -->
+                <div id="content2" style="display: none;">
+                    bla bla bla
+                </div>
             </div>
-
+            <div class="funkyradio-success">
+                <input type="radio" name="radio" id="radio3" />
+                <label for="radio3">Third Option success</label>
+            </div>
+            <div class="funkyradio-danger">
+                <input type="radio" name="radio" id="radio4" />
+                <label for="radio4">Fourth Option danger</label>
+            </div>
+            <div class="funkyradio-warning">
+                <input type="radio" name="radio" id="radio5" />
+                <label for="radio5">Fifth Option warning</label>
+            </div>
+            <div class="funkyradio-info">
+                <input type="radio" name="radio" id="radio6" />
+                <label for="radio6">Sixth Option info</label>
+            </div>
         </div>
-
     </div>
-
-    </body>
     </html>
 </html>
