98c8625c3f2c00b0e7f8e8cd2c085fa173391670
Ignacio <Icm011@alumnos.ucn.cl>
2019-05-13 21:17:24 -0400

    arreglo contacto
    se arreglo la checkbox

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -9,79 +9,98 @@
 {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>--}}
 {{--<!------ Include the above in your HEAD tag ---------->--}}
 
-    <div class="col-md-4">
-        <div class="col-md-6">
-            <h4>Radio Buttons</h4>
+
+
+    <div class="col-md-8">
+        <div class="col-md-12">
+            <h4>Contacto</h4>
             <!-- MENU con checkbox-->
             <div class="funkyradio">
 
                 <div class="funkyradio-default">
-
-
                     <input type="radio" name="radio" class="consulta" id="radio1" value="1"  />
-                    <label for="radio1">First Option default</label>
+                    <label for="radio1">Consulta</label>
                     <!-- Form -->
                     <div id="content" style="display: none;">
-                        <!-- Lo que se muestra del formulario en la página -->
+                        <!-- Lo que se muestra del formulario de consulta-->
+                        <p>Verifique que su consulta no se encuentre en <a href="enlacepagina.html">Preguntas Frecuentes</a></p>
                         <div class="container">
-                            <h2>Horizontal form</h2>
-                            <form class="form-horizontal" action="/action_page.php">
-                                <div class="form-group">
-                                    <label class="control-label col-sm-2" for="email">Email:</label>
-                                    <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-                                    </div>
+                            <div class="col-md-6 col-md-offset-3">
+                                <div class="well well-sm">
+                                    <form class="form-horizontal" action="" method="post">
+                                        <fieldset>
+                                            <!-- Name input-->
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label" for="name">Nombre</label>
+                                                <div class="col-md-9">
+                                                    <input id="name" name="name" type="text" placeholder="Ingrese nombre" class="form-control">
+                                                </div>
+                                            </div>
+
+                                            <!-- Email input-->
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label" for="email">Correo</label>
+                                                <div class="col-md-9">
+                                                    <input id="email" name="email" type="text" placeholder="Ingrese email" class="form-control">
+                                                </div>
+                                            </div>
+
+                                            <!-- Message body -->
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label" for="message">Consulta</label>
+                                                <div class="col-md-9">
+                                                    <textarea class="form-control" id="message" name="message" placeholder="Ingrese consulta aqui..." rows="5"></textarea>
+                                                </div>
+                                            </div>
+
+                                            <!-- Form actions -->
+                                            <div class="form-group">
+                                                <div class="col-md-12 text-right">
+                                                    <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+                                                </div>
+                                            </div>
+                                        </fieldset>
+                                    </form>
                                 </div>
-                                <div class="form-group">
-                                    <label class="control-label col-sm-2" for="pwd">Password:</label>
-                                    <div class="col-sm-10">
-                                        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-sm-offset-2 col-sm-10">
-                                        <div class="checkbox">
-                                            <label><input type="checkbox" name="remember"> Remember me</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-sm-offset-2 col-sm-10">
-                                        <button type="submit" class="btn btn-default">Submit</button>
-                                    </div>
-                                </div>
-                            </form>
+                            </div>
                         </div>
                     </div>
-
                 </div>
+
+                <!--Formulario de Voluntario -->
                 <div class="funkyradio-primary">
                     <input type="radio" name="radio" class="consulta" id="radio2" value="2" />
-                    <label for="radio2">Second Option primary</label>
+                    <label for="radio2">Voluntario</label>
                     <!-- Form -->
                     <div id="content2" style="display: none;">
-                        bla bla bla
+                        <p>voluntariosss</p>
                     </div>
                 </div>
+
+                <!-- Formulario de Denuncias-->
                 <div class="funkyradio-success">
-                    <input type="radio" name="radio" id="radio3" />
-                    <label for="radio3">Third Option success</label>
+                    <input type="radio" name="radio" class="consulta" id="radio3" value="3" />
+                    <label for="radio3">Denuncias</label>
+                    <!-- FORM -->
+                    <div id="content3" style="display: none;">
+                        denuncias
+                    </div>
                 </div>
+
+                <!-- Formulario de Otros-->
                 <div class="funkyradio-danger">
-                    <input type="radio" name="radio" id="radio4" />
-                    <label for="radio4">Fourth Option danger</label>
-                </div>
-                <div class="funkyradio-warning">
-                    <input type="radio" name="radio" id="radio5" />
-                    <label for="radio5">Fifth Option warning</label>
-                </div>
-                <div class="funkyradio-info">
-                    <input type="radio" name="radio" id="radio6" />
-                    <label for="radio6">Sixth Option info</label>
+                    <input type="radio" name="radio" class="consulta" id="Otros" value="4" />
+                    <label for="radio4">Otros</label>
+                    <!-- FORM -->
+                    <div id="content4" style="display: none;">
+                        otrooooos
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+
 
     <!-- Script que hace la funcion de show and hide -->
     <script type="text/javascript">
@@ -92,9 +111,23 @@
                 if (valor == '1'){
                     $("#content").css("display","block");
                     $("#content2").css("display","none");
-                }else {
+                    $("#content3").css("display","none");
+                    $("#content4").css("display","none");
+                }else if(valor == '2') {
                     $("#content").css("display","none");
                     $("#content2").css("display","block");
+                    $("#content3").css("display","none");
+                    $("#content4").css("display","none");
+                }else if(valor == '3'){
+                    $("#content").css("display","none");
+                    $("#content2").css("display","none");
+                    $("#content3").css("display","block");
+                    $("#content4").css("display","none");
+                }else if(valor == '4'){
+                    $("#content").css("display","none");
+                    $("#content2").css("display","none");
+                    $("#content3").css("display","none");
+                    $("#content4").css("display","block");
                 }
             })
 
