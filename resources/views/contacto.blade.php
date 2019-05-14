77c9b1a3638a730f90772f1e7eabb8cf9af06f94
Ignacio <Icm011@alumnos.ucn.cl>
2019-05-14 12:33:52 -0400

    actualizacion contacto 2.1

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -14,10 +14,10 @@
 <div class="mx-auto" style="width: 1000px;">
     <div>
         <div>
-            <h4>Contacto</h4>
+            <h4 style="text-align: center">Contacto</h4>
             <!-- MENU con checkbox-->
 
-            <div class="funkyradio">
+            <div class="funkyradio" align="center">
 
                 <div class="funkyradio-default">
                     <input type="radio" name="radio" class="consulta" id="radio1" value="1"  />
@@ -57,28 +57,27 @@
                         <div class="col-md-6 col-md-offset-3" >
                             <div class="well well-sm">
                                 <form class="form-horizontal" action="" method="post">
-
                                     <fieldset>
                                         <!-- Name input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="name">Nombre</label>
-                                            <div class="col-md-9">
+                                            <p align="left">Nombre</p>
+                                            {{--<label for="name">Nombre</label>--}}
+                                            <div>
                                                 <input id="name" name="name" type="text" placeholder="Ingrese nombre" class="form-control">
                                             </div>
                                         </div>
 
                                         <!-- Email input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label " for="email">Correo</label>
-                                            <div class="col-md-9">
+                                            <p align="left " for="email">Correo</p>
                                                 <input id="email" name="email" type="text" placeholder="Ingrese email" class="form-control">
                                             </div>
                                         </div>
 
                                         <!-- Message body -->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="message">Consulta</label>
-                                            <div class="col-md-9">
+                                            <p align="left " for="message">Consulta</p>
+
                                                 <textarea class="form-control" id="message" name="message" placeholder="Ingrese consulta aqui..." rows="5"></textarea>
                                             </div>
                                         </div>
@@ -97,7 +96,7 @@
                 </div>
 
                 <div id="content2" style="display: none;">
-                    <div class="container">
+                    <div class="container" align="center">
                         <div class="col-md-6 col-md-offset-3">
                             <div class="well well-sm">
                                 <div class="form-horizontal" action="" method="post">
@@ -105,55 +104,55 @@
                                     <fieldset>
                                         <!-- Name input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="name">Nombre</label>
-                                            <div class="col-md-9">
+                                            <p align="left">Nombre</p>
+                                            {{--<label for="name">Nombre</label>--}}
+                                            <div>
                                                 <input id="name" name="name" type="text" placeholder="Ingrese nombre" class="form-control">
                                             </div>
                                         </div>
-
                                         <!-- Rut input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="rut">Rut</label>
-                                            <div class="col-md-9">
+                                            <p align="left">Rut</p>
+                                            <div>
                                                 <input id="rut" name="rut" type="text" placeholder="Ingrese Rut" class="form-control">
                                             </div>
                                         </div>
                                         <!-- Email input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="email">Correo</label>
-                                            <div class="col-md-9">
+                                            <p align="left">Correo</p>
+                                            <div>
                                                 <input id="email" name="email" type="text" placeholder="Ingrese email" class="form-control">
                                             </div>
                                         </div>
 
                                         <!-- Ciudad input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="name">Ciudad</label>
-                                            <div class="col-md-9">
+                                            <p align="left">Ciudad</p>
+                                            <div>
                                                 <input id="ciudad" name="ciudad" type="text" placeholder="Ingrese ciudad" class="form-control">
                                             </div>
                                         </div>
 
                                         <!-- phone input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="email">Telefono (+56912345678)</label>
-                                            <div class="col-md-9">
+                                            <p align="left">Telefono (+56912345678)</p>
+                                            <div>
                                                 <input id="phone" name="phone" type="tel" placeholder="Ingrese Telefono" pattern="+569[0-9]{8}" required class="form-control">
                                             </div>
                                         </div>
 
                                         <!-- Direccion input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="name">Direccion</label>
-                                            <div class="col-md-9">
+                                            <p align="left">Direccion</p>
+                                            <div>
                                                 <input id="name" name="name" type="text" placeholder="Ingrese nombre" class="form-control">
                                             </div>
                                         </div>
 
                                         <!-- Profesion input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="name">Profesion</label>
-                                            <div class="col-md-9">
+                                            <p align="left">Profesion</p>
+                                            <div>
                                                 <input id="profesion" name="profesion" type="text" placeholder="Ingrese profesion" class="form-control">
                                             </div>
                                         </div>
@@ -171,9 +170,10 @@
                         </div>
                     </div>
                 </div>
+
                 <!-- form denuncia -->
                 <div id="content3" style="display: none;">
-                    <div class="container">
+                    <div class="container" align="center">
 
                         <div class="col-md-6 col-md-offset-3" >
                             <div class="well well-sm">
@@ -182,25 +182,25 @@
                                     <fieldset>
                                         <!-- Name input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="name">Nombre</label>
-                                            <div class="col-md-9">
+                                            <p align="left">Nombre</p>
+                                            <div>
                                                 <input id="name" name="name" type="text" placeholder="Ingrese nombre" class="form-control">
                                             </div>
                                         </div>
 
                                         <!-- Email input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label " for="email">Correo</label>
-                                            <div class="col-md-9">
+                                            <p align="left">Correo</p>
+                                            <div>
                                                 <input id="email" name="email" type="text" placeholder="Ingrese email" class="form-control">
                                             </div>
                                         </div>
 
                                         <!-- Message body -->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="message">Consulta</label>
-                                            <div class="col-md-9">
-                                                <textarea class="form-control" id="message" name="message" placeholder="Ingrese consulta aqui..." rows="5"></textarea>
+                                            <p align="left">Denuncia</p>
+                                            <div>
+                                                <textarea class="form-control" id="message" name="message" placeholder="Ingrese su denuncia..." rows="5"></textarea>
                                             </div>
                                         </div>
 
@@ -218,7 +218,7 @@
                 </div>
                 <!-- form otros -->
                 <div id="content4" style="display: none;">
-                    <div class="container">
+                    <div class="container" align="center">
 
                         <div class="col-md-6 col-md-offset-3" >
                             <div class="well well-sm">
@@ -227,25 +227,25 @@
                                     <fieldset>
                                         <!-- Name input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="name">Nombre</label>
-                                            <div class="col-md-9">
+                                            <p align="left">Nombre</p>
+                                            <div>
                                                 <input id="name" name="name" type="text" placeholder="Ingrese nombre" class="form-control">
                                             </div>
                                         </div>
 
                                         <!-- Email input-->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label " for="email">Correo</label>
-                                            <div class="col-md-9">
+                                            <p align="left">Correo</p>
+                                            <div>
                                                 <input id="email" name="email" type="text" placeholder="Ingrese email" class="form-control">
                                             </div>
                                         </div>
 
                                         <!-- Message body -->
                                         <div class="form-group">
-                                            <label class="col-md-3 control-label" for="message">Consulta</label>
-                                            <div class="col-md-9">
-                                                <textarea class="form-control" id="message" name="message" placeholder="Ingrese consulta aqui..." rows="5"></textarea>
+                                            <p align="left">Escriba su mensaje</p>
+                                            <div>
+                                                <textarea class="form-control" id="message" name="message" placeholder="Ingrese mensaje" rows="5"></textarea>
                                             </div>
                                         </div>
 
