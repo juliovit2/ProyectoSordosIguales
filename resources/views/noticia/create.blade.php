ecc39aecad5ab0553458d8ab166a194c03411245
Felipe Varas <felipe.varas@alumnos.ucn.cl>
2019-05-16 02:10:00 -0400

    UPDATED: -Aspecto visual de agregar noticia

--- resources/views/noticia/create.blade.php
+++ resources/views/noticia/create.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="es_ES">
-<head>
-    <title>Agregar Noticia</title>
-    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
-    <script type="text/javascript" src="{{ asset('js/jquery.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/bootstrap.js') }}"></script>
 
-    <link href="{{ asset('summernote/summernote.css') }}" rel="stylesheet">
-    <script type="text/javascript" src="{{ asset('summernote/lang/summernote-es-ES.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('summernote/summernote.js') }}"></script>
-</head>
-<body>
+@extends('layout')
+@section('content')
     <div class="container">
         <div class="row">
             <div class="col-md">
@@ -21,8 +11,8 @@
                         <input type="text" id="titulo" name="titulo" class="form-control">
                     </div>
                     <div class="form-group">
-                        <label for="summernote">Contenido</label>
-                        <textarea id="summernote" name="contenido"></textarea>
+                        <label for="contenido">Contenido</label>
+                        <textarea class="form-control" id="contenido" name="contenido"></textarea>
                     </div>
                     <div class="form-group">
                         <label for="video">Video</label>
@@ -41,20 +31,4 @@
             </div>
         </div>
     </div>
-
-    <script>
-        $(document).ready(function() {
-            $('#summernote').summernote({
-                lang: 'es-ES',
-                toolbar:[
-                    ['style', ['style']],
-                    ['font-style', ['bold', 'italic', 'underline', 'color']],
-                    ['font-style-2', ['fontname', 'fontsize']],
-                    ['paragraph-style', ['paragraph', 'ul', 'ol', 'height']],
-                    ['misc', ['fullscreen']]
-                ]
-            });
-        });
-    </script>
-</body>
-</html>
+@endsection
