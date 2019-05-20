00643085a29392df8a50f05c3360d77d66d985d4
Felipe Varas <felipe.varas@alumnos.ucn.cl>
2019-05-20 17:37:47 -0400

    Retoques Visuales
    UPDATED:
    - Iconos en Index de noticias.
    - Orden de lista en index de noticias.
    - Frecuencia de cambio en carrusel de fotos.
    FIXED:
    - Faltas ortográficas

--- resources/views/noticia/index.blade.php
+++ resources/views/noticia/index.blade.php
@@ -1,29 +1,29 @@
 @extends('layoutGeneral')
 @section('content')
     <div class="col-sm-8">
-
+        <p></p>
         <h2>
             Listado de noticias
-            <a href="{{route('noticia.create')}}" class="btn btn-primary pull-right active"> <i class="glyphicon glyphicon-plus" ></i></a>
         </h2>
+        <a href="{{route('noticia.create')}}" class="btn btn-primary pull-right active" >Agregar Noticia &nbsp;<i class="fas fa-plus"></i></a>
         <table class = "table table-hover table-striped">
             <thead>
 
             <tr>
                 <th width="20px">ID
-                <th>Imagen noticia</th>
-                <th>Titulo Noticia</th>
-                <th>fecha de publicación</th>
+                <th></th>
+                <th>TÍtulo</th>
+                <th>Fecha de publicación</th>
                 <th colspan="3">&nbsp;</th>
             </tr>
             </thead>
             <tbody>
-            @foreach($noticias as $tabla_noticia)
+            @foreach($noticias->reverse() as $tabla_noticia)
                 <tr>
 
                     <td>{{$tabla_noticia->id}}</td>
                     <td>
-                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                        <div id="carousel" class="carousel slide" data-ride="carousel" data-interval="2000">
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
                                     <img width="100px" height="100px" src="{{ asset('storage/logo_fundacion.png')}}" class="d-block w-100" alt="...">
@@ -42,23 +42,23 @@
                     <td>{{$tabla_noticia->created_at}}</td>
                     <td>
                         <a href="{{route('noticia.show',$tabla_noticia->id)}}"
-                           class="btn btn-sm btn-primary active">
-                            <i class="glyphicon glyphicon-eye-open" ></i>
+                           class="btn btn-sm btn-primary active" title="Ver Noticia">
+                            <i class="fas fa-eye"></i>
 
                         </a>
                     </td>
                     <td>
                         <a href="{{route('noticia.show',$tabla_noticia->id)}}"
-                           class="btn btn-sm btn-primary active">
-                            <i class="glyphicon glyphicon-pencil" ></i>
+                           class="btn btn-sm btn-primary active" title="Editar Noticia">
+                            <i class="fas fa-pencil-alt"></i>
 
                         </a>
                     </td>
 
                     <td>
                         <a href="{{route('noticia.show',$tabla_noticia->id)}}"
-                           class="btn btn-sm btn-danger active">
-                            <i class="glyphicon glyphicon-trash" ></i>
+                           class="btn btn-sm btn-danger active"  title="Eliminar Noticia">
+                            <i class="fas fa-trash-alt"></i>
 
                         </a>
                     </td>
@@ -71,8 +71,9 @@
     </div>
     <div class="col-sm-4">
 
-
-
-
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="/js/web.js" type="text/javascript" charset="utf-8" async defer></script>
     </div>
 @endsection
