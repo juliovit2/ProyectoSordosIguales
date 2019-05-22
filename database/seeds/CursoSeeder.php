81a9ca42f2c93f61d565a742da8ee7ac4dfe2c74
Pablo Julio <pjv001@gmail.com>
2019-05-22 17:46:16 -0400

    Creacion de Datos de Prueba con Seeders en tablas relacionadas al curso

---
+++ database/seeds/CursoSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CursoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tabla_cursos')->Insert([
+            'nombre' => 'Lenguaje Señas Basico',
+            'profesorid' => '1'
+        ]);
+
+        DB::table('tabla_cursos')->Insert([
+            'nombre' => 'Lenguaje Señas Intermedio',
+            'profesorid' => '1'
+        ]);
+
+        DB::table('tabla_cursos')->Insert([
+            'nombre' => 'Lenguaje Señas Avansado',
+            'profesorid' => '1'
+        ]);
+    }
+}
