6222008191a8509dfb4694de55245d2d6d3cca76
Julio Díaz <jdm006@alumnos.ucn.cl>
2019-05-30 12:30:44 -0400

    Se corrige error en listar estudiantes. Aún no se puede crear ni editar.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 USE Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        Blade::component('shared._card', 'card');
         Schema::defaultStringLength(191);
     }
 }
