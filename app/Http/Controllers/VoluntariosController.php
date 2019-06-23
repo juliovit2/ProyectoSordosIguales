30689dee7c1ebfc18b5edab758ec15f9af751e52
David <dachovi@outlook.com>
2019-06-23 18:36:25 -0400

    CRUD voluntarios(faltan pruebas)

--- app/Http/Controllers/VoluntariosController.php
+++ app/Http/Controllers/VoluntariosController.php
@@ -14,8 +14,8 @@ class VoluntariosController extends Controller
      */
     public function index()
     {
-        $volutarios = tabla_persona::orderBy('id','ASC')->paginate();
-        return view('InfoContacto.voluntarios',compact('voluntarios'));
+        $voluntarios = tabla_persona::orderBy('id','ASC')->paginate();
+        return view('InfoContacto.index_voluntarios',compact('voluntarios'));
     }
 
     /**
@@ -25,29 +25,103 @@ class VoluntariosController extends Controller
      */
     public function create()
     {
-        //return view('noticia.create'); crear vita crear voluntario
+        return view('InfoContacto.create_voluntarios');
     }
 
 
 
-    public function store(NoticiaStoreRequest $request)
+    public function store(Request $request)
     {
 
+        $this ->validate($request,[
+            'nombre' => 'required',
+            'telefono' => 'required',
+            'rut' => 'required',
+            'correo' => 'required',
+            'certificado' => 'required'
+        ]);
+
         //validar
-        if($request->has('video')) {
-            $this->validate($request, [
-                'video' => 'mimes:mp4,avi,mpeg,flv'
-            ]);
-            $video_path = $request->file('video')->store('public/videos/noticias');
-        }
-        $noticia = new tabla_noticia(array(
-            'titulo' => $request->get('titulo'),
-            'video' => substr ( $video_path , 7, strlen($video_path) -7)));
-
-        $noticia->save();
+        $voluntarios = new tabla_persona(array(
+            'nombre' => $request->get('nombre'),
+            'telefono' => $request->get('telefono'),
+            'rut' => $request->get('rut'),
+            'correo' => $request->get('correo'),
+            'certificado' => $request->get('certificado'),
+            ));
+        $voluntarios->roles()->attach('voluntarios');
+        $voluntarios->save();
 
 
+        return redirect()->route('admin/voluntarios');
+    }
 
-        return redirect()->route('InfoContacto.voluntarios');
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tabla_persona = tabla_persona::find($id);
+        return view('InfoContacto.index_voluntarios',compact('tabla_persona'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $voluntario_a_editar = tabla_persona::find($id);
+        $data = array(
+            'voluntario_a_editar' => $voluntario_a_editar,
+            "is_edit" => true);
+
+        return view('InfoContacto.create_voluntarios')->with('data', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $voluntarioEditado = array(
+            'nombre' => $request->get('nombre'),
+            'telefono' => $request->get('telefono'),
+            'rut' => $request->get('rut'),
+            'correo' => $request->get('correo'),
+            'certificado' => $request->get('certificado'),
+            'rol' => $request->get('rol')
+        );
+
+        DB::table('tabla_persona')
+            ->where('id', $id)
+            ->update($voluntarioEditado);
+
+        return redirect()->route('InfoContacto.index_voluntarios');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $voluntario_a_eliminar = tabla_persona::find($id);
+        $voluntario_a_eliminar->delete();
+
+        return redirect()->route('InfoContacto.index_voluntarios');
     }
 }
---
+++ resources/views/InfoContacto/create_voluntarios.blade.php
@@ -0,0 +1,42 @@
+@extends('layoutGeneral')
+@section('content')
+
+    <script type="text/javascript" src="{{ URL::asset('js/summernote-es-ES.js') }}"></script>
+    <div class="container mt-5 mb-5 containerForm">
+        <div class="row">
+            <div class="col-md">
+                <form id="form" action="" method="POST" enctype="multipart/form-data">
+                    <input type="hidden" name="_token" value="{!! csrf_token() !!}">
+                    <div class="form-group">
+                        <label for="Nombre"><h4>Nombre</h4></label>
+                        <input type="text" id="nombre" name="nombre" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="Telefono"><h4>Telefono</h4></label>
+                        <input type="text" id="telefono" name="telefono" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="Rut"><h4>Rut</h4></label>
+                        <input type="text" id="rut" name="rut" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="Correo"><h4>Correo</h4></label>
+                        <input type="text" id="correo" name="correo" class="form-control">
+                    </div>
+
+
+                    <div class=form-group">
+                        <input id="but" type="button" value="Agregar Voluntario" class="btn btn-primary">
+                    </div>
+                    <button onclick="javascript:history.back()" id="volver">Volver</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+@endsection
