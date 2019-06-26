560241f9ba370fbcd0e1ec9d49b8772cd100aa69
Cristian <theberzerker_15_@hotmail.com>
2019-06-26 11:02:41 -0400

    Ajustes FAQ

--- resources/views/InfoContacto/edit_faq.blade.php
+++ resources/views/InfoContacto/edit_faq.blade.php
@@ -1,9 +1,10 @@
 @extends('layoutGeneral')
 
 @section('content')
-    <div class="row">
-        <section class="content">
-            <div class="col-md-8 col-md-offset-2">
+    <div class="container mt-5 mb-5 containerForm">
+        <div class="row">
+            <div class="col-md">
+
                 @if (count($errors) > 0)
                     <div class="alert alert-danger">
                         <strong>Error!</strong> Revise los campos obligatorios.<br><br>
@@ -19,69 +20,57 @@
                         {{Session::get('success')}}
                     </div>
                 @endif
-
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Nueva Pregunta</h3>
-                    </div>
-                    <div class="panel-body">
-                        <div class="table-container">
-
-                            <form method="POST" action="{{ route('faq.update',$pregunta->id) }}"  role="form">
-                                {{ csrf_field() }}
-                                <input name="_method" type="hidden" value="PATCH">
-                                <div class="row">
-                                    <div class="col-xs-6 col-sm-6 col-md-6">
-                                        <div class="form-group">
-                                            <input type="text" name="pregunta" id="pregunta" class="form-control input-sm" value="{{$pregunta->pregunta}}">
-                                        </div>
-                                    </div>
-                                    <div class="col-xs-6 col-sm-6 col-md-6">
-                                        <div class="form-group">
-                                           <input name="respuesta" onchange="getId(this.value)" id="respuesta"  class="form-control input-sm" value="{{$pregunta->respuesta}}">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-
-                                    <div class="col-xs-12 col-sm-12 col-md-12">
-                                        
-                                        <a href="#edit" class="btn btn-primary" data-toggle="modal">Actualizar</a>
-                                        <a href="{{ route('faq.index') }}" class="btn btn-sm btn-primary active" >Atrás</a>
-
-                                        <!--pop up confirmacion -->
-                                        <div class="modal fade" id="edit">
-                                            <div class="modal-dialog">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <h5 class="modal-title">Confirmacion</h5>
-                                                        <button tyle="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <p>Si presiona cancelar, no se guardaran los cambios</p>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
-                                                        <button type="submit" value="Actualizar" class="btn btn-primary active">Guardar Cambios</button>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!-- endf pop up-->
-
-                                    </div>
-
-                                </div>
-                            </form>
-
+                <form method="POST" action="{{ route('faq.update',$pregunta->id) }}"  role="form">
+                    {{ csrf_field() }}
+                    <input name="_method" type="hidden" value="PATCH">
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <input type="text" name="pregunta" id="pregunta" class="form-control input-sm" value="{{$pregunta->pregunta}}">
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <input name="respuesta" onchange="getId(this.value)" id="respuesta"  class="form-control input-sm" value="{{$pregunta->respuesta}}">
+                            </div>
                         </div>
                     </div>
 
-                </div>
+                    <div class="row">
+
+                        <div class="col-xs-12 col-sm-12 col-md-12">
+
+                            <a href="#edit" class="btn btn-primary" data-toggle="modal">Actualizar</a>
+                            <a href="{{ route('faq.index') }}" class="btn btn-primary" >Atrás</a>
+
+                            <!--pop up confirmacion -->
+                            <div class="modal fade" id="edit">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title">Confirmacion</h5>
+                                            <button tyle="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Si presiona cancelar, no se guardaran los cambios</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
+                                            <button type="submit" value="Actualizar" class="btn btn-primary active">Guardar Cambios</button>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- endf pop up-->
+
+                        </div>
+
+                    </div>
+                </form>
             </div>
-        </section>
+        </div>
+    </div>
 
         <!-- Bootstrap core JavaScript
     ================================================== -->
