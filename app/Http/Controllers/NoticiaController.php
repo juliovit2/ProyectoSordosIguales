3a9a7a6062680de17caafd4644975162467e7bd0
denis.miranda@alumnos.ucn.cl <denis.miranda@alumnos.ucn.cl>
2019-07-18 13:57:08 -0400

    FIX: error descomentar lineas de codigo

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -20,9 +20,9 @@ class NoticiaController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    // public function __construct() {
-    //     $this->middleware('auth');
-    // }
+    public function __construct() {
+        $this->middleware('auth');
+    }
 
     public function index()
     {
